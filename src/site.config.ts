53b46fc7a9b5e63b89852c4d7b8dcd3da3549b3c
xlinkexe <2457444033@qq.com>
2025-10-09 12:03:02 +0800

    fix(waline): 更新Waline服务端地址为有效链接

--- src/site.config.ts
+++ src/site.config.ts
@@ -155,7 +155,7 @@ export const integ: IntegrationUserConfig = {
   waline: {
     enable: true,
     // Server service link
-    server: '',
+    server: 'https://waline.ljx.icu',
     // Refer https://waline.js.org/en/guide/features/emoji.html
     emoji: ['bmoji', 'weibo'],
     // Refer https://waline.js.org/en/reference/client/props.html
