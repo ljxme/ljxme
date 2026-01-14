de5a097661a4ea8c9ea01c1e1c19f1d3b6ccf8b6
Ljxme <ljx_me@qq.com>
2026-01-14 14:43:12 +0800

    fix(waline): add missing type declaration ignores for .astro modules

--- src/components/waline/index.ts
+++ src/components/waline/index.ts
@@ -1,3 +1,6 @@
+// @ts-ignore: module or its type declaration for .astro may be missing
 export { default as Comment } from './Comment.astro'
+// @ts-ignore: module or its type declaration for .astro may be missing
 export { default as PageInfo } from './PageInfo.astro'
+// @ts-ignore: module or its type declaration for .astro may be missing
 export { default as Pageview } from './Pageview.astro'
